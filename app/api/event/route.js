288ecda0f29c587719e6ea5182bfb52f9bcbcaa1
g00gol <jackey2870@gmail.com>
2024-04-24 13:30:00 -0400

    progress

--- app/api/event/route.js
+++ app/api/event/route.js
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import { createEvent } from "./data";
 
 export async function POST(req) {
@@ -5,8 +6,15 @@ export async function POST(req) {
 
   const name = data.get("name");
   const meeting_pattern = data.get("meeting_pattern");
+  if (!name || !meeting_pattern) {
+    return redirect("/?error=Missing name or meeting pattern.");
+  }
+
   if (meeting_pattern === "week") {
     const days = data.getAll("days");
+    if (!days.length) {
+      return redirect("/?error=Must select at least 1 day.");
+    }
     const id = await createEvent({ name, meeting_pattern, days });
     return Response.json(id);
   }
--- app/components/Home/CreateEvent.jsx
+++ app/components/Home/CreateEvent.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+
 import Week from "@/app/components/Home/Week";
 import Month from "@/app/components/Home/Month";
 
 export default function CreateEvent() {
+  const errorParams = useSearchParams();
+  const error = errorParams.get("error");
+
   const [meetingPattern, setMeetingPattern] = useState("week");
   const [selectedDays, setSelectedDays] = useState([]);
 
@@ -11,36 +16,46 @@ export default function CreateEvent() {
   }
 
   return (
-    <form
-      method="POST"
-      action="/api/event"
-      className="flex h-screen w-screen flex-col space-y-6 rounded-md border border-neutral bg-neutral p-8 lg:h-auto lg:w-fit"
-    >
-      <span className="space-y-2 lg:space-x-2">
-        <input
-          className="input"
-          type="text"
-          name="name"
-          placeholder="Event Name"
-        />
-        <select
-          name="meeting_pattern"
-          className="select"
-          onChange={(e) => handleMeetingPatternChange(e)}
-          value={meetingPattern}
-        >
-          <option value="week">Week</option>
-          <option value="month">Month</option>
-        </select>
-      </span>
-      {meetingPattern === "week" ? (
-        <Week selectedDays={selectedDays} setSelectedDays={setSelectedDays} />
-      ) : (
-        meetingPattern === "month" && <Month />
+    <>
+      <form
+        method="POST"
+        action="/api/event"
+        className="flex h-screen w-screen flex-col space-y-6 rounded-md border border-neutral bg-neutral p-8 lg:h-auto lg:w-fit"
+      >
+        <span className="space-y-2 lg:space-x-2">
+          <input
+            className="input"
+            type="text"
+            name="name"
+            placeholder="Event Name"
+          />
+          <select
+            name="meeting_pattern"
+            className="select"
+            onChange={(e) => handleMeetingPatternChange(e)}
+            value={meetingPattern}
+          >
+            <option value="week">Week</option>
+            <option value="month">Month</option>
+          </select>
+        </span>
+        {meetingPattern === "week" ? (
+          <Week selectedDays={selectedDays} setSelectedDays={setSelectedDays} />
+        ) : (
+          meetingPattern === "month" && <Month />
+        )}
+        <button className="btn" type="submit">
+          Create
+        </button>
+      </form>
+
+      {error && (
+        <div className="toast toast-end">
+          <div className="alert alert-error">
+            <span>{error}</span>
+          </div>
+        </div>
       )}
-      <button className="btn" type="submit">
-        Create
-      </button>
-    </form>
+    </>
   );
 }
